2547bf5439f44b65e70ad2fd5f69a4a0df31dff8
Ben Governali <bgovern93@gmail.com>
2018-08-28 17:30:00 -0400

    Limited side of grid container so no spaces appear between grid elements when increasing window size. Added functions to change background-colors of grid elements

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,9 +2,17 @@ let i = 0;
 const container = document.querySelector('#container');
 
 while (i < 256) {
-  const div = document.createElement('div');
-  div.setAttribute('class', 'gridItem');
-  div.setAttribute('style', 'width: 50px; height: 50px;')
+  let div = document.createElement('div');
+  div.setAttribute('class', 'gridItemAlpha');
+  div.setAttribute('style', 'width: 50px; height: 50px; border-width: 1px; border-color: black; border-style: solid');
   container.appendChild(div);
   i += 1;
+}
+
+function shadeGray() {
+  div.setAttribute('style', 'background-color: gray');	
+}
+
+function shadeBlack() {
+  div.setAttribute('style', 'backgrounf-color: black');	
 }
